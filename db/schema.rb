628c1fdf09dde62f9588b2e80f37eeb0c460ee40
Thomas Girard <thomas.girard@hey.com>
2023-10-01 07:44:50 -0400

    feat: install all missing CI deps. + format all code

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_09_30_215423) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 20_230_930_215_423) do
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'link'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
