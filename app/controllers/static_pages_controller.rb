3d45ab1d0912eb62eb0cfc2782784fa68e5dd693
Tristan-OD <mihijitod@gmail.com>
2023-10-29 10:27:55 -0400

    feat: posts ui

--- app/controllers/static_pages_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class StaticPagesController < ApplicationController
-  def home
-    @posts = Post.all
-  end
-end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  before_save :get_link_description
+  before_save :get_post_description
+  after_initialize :get_post_image
   attr_accessor :image
 
-  def get_link_description
+  def get_post_description
     page = MetaInspector.new(self.link)
     self.description = page.best_description
+  end
+
+  def get_post_image
+    begin
+      page = MetaInspector.new(self.link)
+      self.image = page.images.best
+    rescue
+      self.image = nil
+    end
   end
 end
