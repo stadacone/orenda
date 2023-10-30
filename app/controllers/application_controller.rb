7ca85ec46aefb6ec5c50f54033bc309ef34a91dc
Thomas Girard <thomas.girard@hey.com>
2023-10-29 21:40:22 -0400

    refactor: extract permission checking to an Authotization concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,5 @@
 
 class ApplicationController < ActionController::Base
   include Authentication
-  before_action :verify_permissions
-
-  def verify_permissions
-    permission = Permission.find_by(resource: controller_name, action: action_name)
-    unless permission.in? current_user&.permissions
-      flash[:error] = "You don't have permission to perform this action on this resource."
-      redirect_to root_url, status: :forbidden
-    end
-  end
+  include Authorization
 end
