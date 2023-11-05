08601fb00352228576431572839b67784c620001
Tristan-OD <mihijitod@gmail.com>
2023-11-05 12:31:49 -0500

    feat: better placeholder

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  before_save :get_post_description
+  before_save :get_post_metadata
   belongs_to :user
 
-  validates :link, format: {with: URI::DEFAULT_PARSER.make_regexp}
+  validates :link, format: { with: URI::DEFAULT_PARSER.make_regexp }
 
-  def get_post_description
-    begin
-      page = MetaInspector.new(self.link)
-      self.description = page.best_description
-      self.image_source = page.images.best
-    rescue
-      self.description = nil
-      self.image_source = nil
-    end
+  def get_post_metadata
+    page = MetaInspector.new(link)
+    self.description = page.best_description
+    self.image_source = page.images.best
+  rescue
+    self.description = nil
+    self.image_source = nil
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_31_123906) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_05_170903) do
   create_table "active_sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_31_123906) do
     t.string "description"
     t.string "image_source"
     t.integer "user_id", null: false
+    t.string "favicon_source"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
